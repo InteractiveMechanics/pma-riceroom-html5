7f21d08aceb48ff20c34d7cef80bb36788e894f8
Michael Tedeschi <mike.tedeschi@gmail.com>
2015-10-30 13:27:43 -0400

    Add automation with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        sass: {
+            dev: {
+                files: {
+                    'css/main.css': 'css/sass/main.scss'
+                }
+            }
+        },
+        jekyll: {
+            dev: {
+                options: {
+                    src: 'jekyll-data',
+                    dest: 'jekyll-data/build',
+                    config: 'jekyll-data/_config.yml'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['**/*.scss'], 
+                tasks: ['sass'] 
+            },
+            jekyll: {
+                files: ['jekyll-data/**/*.yml', 'jekyll-data/*.xml', 'jekyll-data/*.json'],
+                tasks: ['jekyll']
+            }
+		}
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jekyll');
+    
+    grunt.registerTask('default', ['watch']);
+};
